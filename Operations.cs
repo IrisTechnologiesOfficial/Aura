caced57a6b1e07943178f944687ce07c0d7606d0
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-05-07 01:02:37 +0200

    Create Operations.cs

---
+++ Operations.cs
@@ -0,0 +1,10 @@
+//Copyright 2019 Iris Technologies, All Rights Reserved
+namespace AuraEngine
+{
+    internal static class Operations
+    {
+        internal const string CheckPing = "????";
+
+        internal const string RequestFile = "?????";
+    }
+}
